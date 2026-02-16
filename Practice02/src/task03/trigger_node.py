5c8616f7cc6e8c9097de9095cca1cb97f77e6122
ilakondakov <kondakov.ilya1@gmail.com>
2026-02-16 19:58:56 +0200

    task3 fix 5

--- Practice02/src/task03/trigger_node.py
+++ Practice02/src/task03/trigger_node.py
@@ -1,3 +1,5 @@
+import time
+
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Trigger
@@ -17,34 +19,45 @@ class TriggerNode(Node):
         )
         self.stored_string = self.default_string
 
-        self.trigger_client = self.create_client(Trigger, '/spgc/trigger')
-        self.stored_string = self._load_trigger_message()
-
         self.service = self.create_service(
             Trigger,
             self.service_name,
             self.trigger_callback,
         )
 
-    def _load_trigger_message(self) -> str:
-        if not self.trigger_client.wait_for_service(timeout_sec=1.0):
-            return self.default_string
+        self.trigger_client = self.create_client(Trigger, '/spgc/trigger')
+        self._trigger_call_done = False
+        self._trigger_deadline = time.monotonic() + 5.0
+        self._trigger_timer = self.create_timer(0.1, self.try_trigger_call)
 
-        future = self.trigger_client.call_async(Trigger.Request())
-        rclpy.spin_until_future_complete(self, future, timeout_sec=3.0)
+    def try_trigger_call(self) -> None:
+        if self._trigger_call_done:
+            return
 
-        if not future.done():
-            return self.default_string
+        if not self.trigger_client.wait_for_service(timeout_sec=0.0):
+            if time.monotonic() >= self._trigger_deadline:
+                self._trigger_call_done = True
+                self._trigger_timer.cancel()
+            return
 
+        self._trigger_call_done = True
+        self._trigger_timer.cancel()
+
+        try:
+            future = self.trigger_client.call_async(Trigger.Request())
+        except Exception:
+            return
+
+        future.add_done_callback(self.on_trigger_response)
+
+    def on_trigger_response(self, future) -> None:
         try:
             response = future.result()
         except Exception:
-            return self.default_string
+            return
 
-        if response is None:
-            return self.default_string
-
-        return response.message
+        if response is not None:
+            self.stored_string = response.message
 
     def trigger_callback(self, request: Trigger.Request, response: Trigger.Response):
         _ = request
